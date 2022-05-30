fc8436769ef94b64f74ac0cb36e02bfc9e90fa3f
Amir Tuval <amirtu@codota.com>
2022-05-30 21:22:15 +0300

    record agent stats and print every 10 secs

--- gateway/app.js
+++ gateway/app.js
@@ -8,23 +8,35 @@ const app = express();
 const SERVICE_URL = process.env.SERVICE_URL;
 const agent = SERVICE_URL.startsWith('https://') ? new https.Agent({ keepAlive: true, maxSockets: Number.MAX_VALUE }) : new http.Agent({ keepAlive: true, maxSockets: Number.MAX_VALUE });
 
+const stats = {
+  connectionOpen: 0,
+  connectionKeptAlive: 0,
+  connectionReused: 0
+};
+
 const oldCreateConnection = agent.createConnection;
 agent.createConnection = function () {
-    console.log('creating agent connection');
+    //console.log('creating agent connection');
+    stats.connectionOpen++;
     return oldCreateConnection.apply(agent, arguments);
 }
 const oldKeepSocketAlive = agent.keepSocketAlive;
 agent.keepSocketAlive = function () {
-    console.log('keeping agent socket alive');
+    //console.log('keeping agent socket alive');
+    stats.connectionKeptAlive++;
     return oldKeepSocketAlive.apply(agent, arguments);
 }
 
 const oldReuseSocket = agent.reuseSocket;
 agent.reuseSocket = function () {
-    console.log('reusing socket alive');
+    //console.log('reusing socket alive');
+    stats.connectionReused++;
     return oldReuseSocket.apply(agent, arguments);
 }
 
+setInterval(() => {
+    console.log(stats);
+}, 10000);
 function socketsToObj(sockets) {
     return {
         domainCount: Object.keys(sockets).length,
