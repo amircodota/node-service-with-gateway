3e9ef3c28145f178905b71cf286509da8d17b23d
Amir Tuval <amirtu@codota.com>
2022-05-30 20:41:59 +0300

    print agent stats

--- gateway/app.js
+++ gateway/app.js
@@ -8,6 +8,13 @@ const app = express();
 const SERVICE_URL = process.env.SERVICE_URL;
 const agent = SERVICE_URL.startsWith('https://') ? new https.Agent({ keepAlive: true, maxSockets: Number.MAX_VALUE }) : new http.Agent({ keepAlive: true, maxSockets: Number.MAX_VALUE });
 
+function socketsToObj(sockets) {
+    return {
+        domainCount: Object.keys(sockets).length,
+        count: Object.values(sockets).map(x => x.length).reduce((a, b) => a + b, 0)
+    };
+}
+
 app.use('/service', createProxyMiddleware({
     target: SERVICE_URL,
     proxyTimeout: 30000,
@@ -17,6 +24,11 @@ app.use('/service', createProxyMiddleware({
     secure: process.env.SECURE !== 'false',
     agent,
     onProxyRes(proxyRes, req) {
+        console.log(`PROXY agent stats`, {
+            sockets: socketsToObj(agent.sockets),
+            freeSockets: socketsToObj(agent.freeSockets),
+            requests: socketsToObj(agent.requests),
+        });
         console.log(`PROXY request`, req.headers);
         console.log('PROXY res:', proxyRes.headers);
     }
