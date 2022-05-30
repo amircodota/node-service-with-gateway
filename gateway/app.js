e994b2b6302482eef233debab243e683399ae4cb
Amir Tuval <amirtu@codota.com>
2022-05-30 16:34:34 +0300

    set keepAlive: true for all connections

--- gateway/app.js
+++ gateway/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const {createProxyMiddleware} = require("http-proxy-middleware");
 const https = require("https");
+const http = require("http");
 
 const app = express();
 
-//const agent = new https.Agent({ keepAlive: true });
+const SERVICE_URL = process.env.SERVICE_URL;
+const agent = SERVICE_URL.startsWith('https://') ? new https.Agent({ keepAlive: true }) : new http.Agent({ keepAlive: true });
 
 app.use('/service', createProxyMiddleware({
-    target: process.env.SERVICE_URL,
+    target: SERVICE_URL,
     proxyTimeout: 30000,
     timeout: 30000,
     changeOrigin: true,
     xfwd: true,
     secure: true,
-    //agent
+    agent
 }));
 
 app.listen(process.env.PORT || 3001);
