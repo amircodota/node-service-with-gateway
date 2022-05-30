7d433372273a549d8fa29a36e7ef90010e17907b
Amir Tuval <amirtu@codota.com>
2022-05-30 15:24:11 +0300

    fix gateway

--- gateway/app.js
+++ gateway/app.js
@@ -6,7 +6,10 @@ const app = express();
 app.use('/service', createProxyMiddleware({
     target: process.env.SERVICE_URL,
     proxyTimeout: 30000,
-    timeout: 30000
+    timeout: 30000,
+    changeOrigin: true,
+    xfwd: true,
+    secure: true
 }));
 
 app.listen(process.env.PORT || 3001);
